45d4dcfc4cf8912b3740b937c17d80fe0702cf9b
박신영 <skeicg12@gmail.com>
2023-09-11 19:26:34 +0900

    0911 좌석 업데이트, 주변식당 업데이트(js 추가해야됨)

--- 최종/박신영/assets/js/reservation.js
+++ 최종/박신영/assets/js/reservation.js
@@ -21,127 +21,14 @@ document.querySelector("#admin_class").addEventListener("click", function () {
     }
 })
 
-// 페이지 버튼 
 
-for(let i = 0; i < 5; i++){
-    let pagedom = document.querySelectorAll(".pageNum");
-    pagedom[i].addEventListener("click",function(event){
-        let j = i +1;
-           if(event.target == pagedom[i]){
-            document.getElementById("pageNum"+1).style.backgroundColor='rgb(243, 243, 243)';    
-            document.getElementById("pageNum"+1).style.color='#000000';
-            document.getElementById("pageNum"+2).style.backgroundColor='';    
-            document.getElementById("pageNum"+2).style.color='#000000';
-            document.getElementById("pageNum"+3).style.backgroundColor='';    
-            document.getElementById("pageNum"+3).style.color='#000000';
-            document.getElementById("pageNum"+4).style.backgroundColor='';    
-            document.getElementById("pageNum"+4).style.color='#000000';
-            document.getElementById("pageNum"+5).style.backgroundColor='';    
-            document.getElementById("pageNum"+5).style.color='#000000';
-
-            document.getElementById("pageNum"+j).style.backgroundColor='rgb(34, 50, 65)';    
-            document.getElementById("pageNum"+j).style.color='#ffffff';  
-           } 
-            
-    })
-}
-
-// select 요소와 input 요소에 대한 참조 가져오기
+        // select 요소와 input 요소에 대한 참조 가져오기
         var selectElement = document.getElementById("seat_select");
         var inputElement = document.getElementById("seat_input");
-
-
-        // select 요소에 이벤트 리스너 추가하기
-        selectElement.addEventListener("change", function () {
-            // 선택된 옵션 텍스트 가져오기
-            var selectedOptionText = selectElement.options[selectElement.selectedIndex].text;
-
-            // 선택된 옵션 텍스트가 "강의실 선택"이 아닌 경우
-            if (selectedOptionText !== "강의실 선택") {
-                // input 요소의 값을 20으로 설정하기
-                inputElement.value = "20";
-            } else {
-                // "강의실 선택" 옵션이 선택된 경우, input 요소의 값을 0으로 설정하기
-                inputElement.value = "0";
-            }
-        });
         var plusButton = document.getElementById("plus");
         var minusButton = document.getElementById("minus");
-
-        // 더하기(+) 버튼과 빼기(-) 버튼 초기 상태: 비활성화
-        plusButton.disabled = true;
-        minusButton.disabled = true;
-
-        // select 요소에 이벤트 리스너 추가
-        selectElement.addEventListener("change", function () {
-            var selectedOptionText = selectElement.options[selectElement.selectedIndex].text;
-
-            // "강의실 선택" 이외의 옵션을 선택하면 버튼 활성화, 그렇지 않으면 비활성화
-            if (selectedOptionText !== "강의실 선택") {
-                plusButton.disabled = false;
-                minusButton.disabled = false;
-            } else {
-                plusButton.disabled = true;
-                minusButton.disabled = true;
-                inputElement.value = "0"; // "강의실 선택"일 때 값 0으로 초기화
-            }
-        });
-
-        // 더하기(+) 버튼 클릭 이벤트 리스너 추가
-        plusButton.addEventListener("click", function () {
-            var currentValue = parseInt(inputElement.value);
-            if (currentValue < 30) {
-                inputElement.value = (currentValue + 1).toString();
-            } else {
-                alert("최대 좌석은 30석 입니다.");
-            }
-        });
-
-        // 빼기(-) 버튼 클릭 이벤트 리스너 추가
-        minusButton.addEventListener("click", function () {
-            var currentValue = parseInt(inputElement.value);
-            if (currentValue > 10) {
-                inputElement.value = (currentValue - 1).toString();
-            } else {
-                alert("최소 좌석은 10석 입니다.");
-            }
-        });
-
-        // seat_input의 값이 변경될 때 이벤트 리스너 추가
-        inputElement.addEventListener("change", function () {
-            var currentValue = parseInt(inputElement.value);
-            if (currentValue > 30) {
-                alert("최대 좌석은 30석 입니다.");
-                inputElement.value = "30"; // 값이 30을 초과하면 30으로 설정
-            } else if (currentValue < 10) {
-                alert("최소 좌석은 10석 입니다.");
-                inputElement.value = "10"; // 값이 10 미만이면 10으로 설정
-            }
-        });
-
-
-
-
-
-        var selectedRoom = ''; // 현재 선택된 강의실 이름
-        var selectedSeats = 0; // 현재 선택된 강의실의 좌석 수
         var correctionButton = document.getElementById("correction");
 
-
-        // select 요소에 이벤트 리스너 추가하기
-        selectElement.addEventListener("change", function () {
-            // 선택된 옵션 텍스트 가져오기
-            var selectedOptionText = selectElement.options[selectElement.selectedIndex].text;
-
-            // 선택된 옵션 텍스트가 "강의실 선택"이 아닌 경우
-            if (selectedOptionText !== "강의실 선택") {
-                // input 요소의 값을 20으로 설정하기
-                inputElement.value = "20";
-            } else {
-                // "강의실 선택" 옵션이 선택된 경우, input 요소의 값을 0으로 설정하기
-                inputElement.value = "0";
-            }
-        });
         // 각 강의실에 대한 정보를 객체로 저장
         var classrooms = {
             "1강의실": { seats: 20 },
@@ -150,33 +37,202 @@ for(let i = 0; i < 5; i++){
             "4강의실": { seats: 20 }
         };
 
-        // "수정" 버튼 클릭 이벤트 리스너
-        correctionButton.addEventListener("click", function () {
-            var selectedOptionText = selectElement.options[selectElement.selectedIndex].text;
-            var inputValue = parseInt(inputElement.value);
-
-            if (selectedOptionText !== "강의실 선택" && inputValue >= 0) {
-                // 선택된 강의실 정보 업데이트
-                classrooms[selectedOptionText].seats = inputValue;
-
-                // 알림으로 수정된 좌석 수 표시
-                alert(selectedOptionText + " 강의실의 좌석이 " + inputValue + "개로 수정되었습니다.");
-            }
-        });
-
-        // 선택된 강의실이 변경될 때 select 요소에 이벤트 리스너 추가
+        // select 요소에 이벤트 리스너 추가
         selectElement.addEventListener("change", function () {
             var selectedOptionText = selectElement.options[selectElement.selectedIndex].text;
 
             if (selectedOptionText !== "강의실 선택") {
                 // 선택된 강의실의 좌석 수를 입력 필드에 표시
                 inputElement.value = classrooms[selectedOptionText].seats;
+                plusButton.disabled = false;
+                minusButton.disabled = false;
             } else {
                 inputElement.value = "0";
+                plusButton.disabled = true;
+                minusButton.disabled = true;
             }
         });
 
-          // <main head>
+        // 더하기(+) 버튼 클릭 이벤트 리스너 추가
+        plusButton.addEventListener("click", function () {
+            var selectedOptionText = selectElement.options[selectElement.selectedIndex].text;
+            var inputValue = parseInt(inputElement.value);
+
+            if (selectedOptionText !== "강의실 선택" && inputValue < 28) {
+                inputElement.value = (inputValue + 1).toString();
+            } else {
+                alert("최대 좌석은 28석 입니다.");
+            }
+        });
+
+        // 빼기(-) 버튼 클릭 이벤트 리스너 추가
+        minusButton.addEventListener("click", function () {
+            var selectedOptionText = selectElement.options[selectElement.selectedIndex].text;
+            var inputValue = parseInt(inputElement.value);
+
+            if (selectedOptionText !== "강의실 선택" && inputValue > 10) {
+                inputElement.value = (inputValue - 1).toString();
+            } else {
+                alert("최소 좌석은 10석 입니다.");
+            }
+        });
+
+        // "수정" 버튼 클릭 이벤트 리스너
+        correctionButton.addEventListener("click", function () {
+            var selectedOptionText = selectElement.options[selectElement.selectedIndex].text;
+            var inputValue = parseInt(inputElement.value);
+            var seatButtonContainer = document.querySelector(".Bleft");
+
+            if (selectedOptionText !== "강의실 선택" && inputValue >= 0) {
+                // 선택된 강의실 정보 업데이트
+                classrooms[selectedOptionText].seats = inputValue;
+
+                // 좌석 버튼 그룹을 업데이트
+                seatButtonContainer.innerHTML = ""; // 기존 버튼 제거
+
+                // 변경된 좌석 수만큼 좌석 버튼을 생성하여 추가
+                for (var i = 1; i <= inputValue; i++) {
+                    var seatButton = document.createElement("input");
+                    seatButton.type = "button";
+                    seatButton.className = "seat-button";
+                    seatButton.value = i.toString();
+                    seatButtonContainer.appendChild(seatButton);
+                }
+
+                // 알림으로 수정된 좌석 수 표시
+                alert(selectedOptionText + " 강의실의 좌석이 " + inputValue + "개로 수정되었습니다.");
+            }
+        });
+
+        // JavaScript로 현재 날짜를 가져오는 함수
+        function getCurrentDate() {
+            var today = new Date();
+            var year = today.getFullYear();
+            var month = (today.getMonth() + 1).toString().padStart(2, '0'); // 월은 0부터 시작하므로 1을 더하고 두 자리로 만듭니다.
+            var date = today.getDate().toString().padStart(2, '0'); // 일자를 두 자리로 만듭니다.
+
+            return { year: year, month: month, date: date };
+        }
+
+        // 연도 옵션 초기화
+        function initializeYearOptions() {
+            var yearSelect = document.getElementById('yearSelect');
+            var currentDate = getCurrentDate();
+
+            // 현재 연도를 기준으로 과거 5년부터 미래 5년까지의 연도 옵션 초기화
+            for (var i = currentDate.year - 5; i <= currentDate.year + 5; i++) {
+                var option = document.createElement('option');
+                option.value = i;
+                option.textContent = i + '년'; // "년" 추가
+                yearSelect.appendChild(option);
+            }
+        }
+
+        // 월별 일자 옵션 초기화
+        function updateDayOptions() {
+            var yearSelect = document.getElementById('yearSelect');
+            var monthSelect = document.getElementById('monthSelect');
+            var dateSelect = document.getElementById('dateSelect');
+
+            // 선택된 연도와 월 가져오기
+            var selectedYear = parseInt(yearSelect.value);
+            var selectedMonth = parseInt(monthSelect.value);
+
+            // 해당 월의 일자 옵션 초기화
+            dateSelect.innerHTML = ''; // 기존 옵션 삭제
+
+            var maxDays;
+            if ([1, 3, 5, 7, 8, 10, 12].includes(selectedMonth)) {
+                maxDays = 31;
+            } else if ([4, 6, 9, 11].includes(selectedMonth)) {
+                maxDays = 30;
+            } else {
+                // 2월은 윤년을 고려하여 28 또는 29일
+                maxDays = (selectedYear % 4 === 0 && (selectedYear % 100 !== 0 || selectedYear % 400 === 0)) ? 29 : 28;
+            }
+
+            for (var i = 1; i <= maxDays; i++) {
+                var option = document.createElement('option');
+                var date = i.toString().padStart(2, '0');
+                option.value = date;
+                option.textContent = date + '일'; // "일" 추가
+                dateSelect.appendChild(option);
+            }
+        }
+
+        // 페이지가 로드될 때 초기화
+        window.onload = function () {
+            var yearSelect = document.getElementById('yearSelect');
+            var monthSelect = document.getElementById('monthSelect');
+            var dateSelect = document.getElementById('dateSelect');
+
+            // 연도 옵션 초기화
+            initializeYearOptions();
+
+            // 월 옵션 초기화
+            for (var i = 1; i <= 12; i++) {
+                var option = document.createElement('option');
+                var month = i.toString().padStart(2, '0');
+                option.value = month;
+                option.textContent = month + '월'; // "월" 추가
+                monthSelect.appendChild(option);
+            }
+
+            // 현재 날짜 선택
+            var currentDate = getCurrentDate();
+            yearSelect.value = currentDate.year;
+            monthSelect.value = currentDate.month;
+
+            // 일자 옵션 초기화
+            updateDayOptions();
+
+            // 현재 일자 선택
+            dateSelect.value = currentDate.date;
+        }
+
+
+
+        // 좌석 버튼 클릭 시 동작
+        var seatButtons = document.querySelectorAll('.seat-button');
+        var seatNum = document.getElementById('seatNum');
+        var seatNumCheck = document.getElementById('seatNumCheck');
+     
+
+        // 좌석 버튼 클릭 시 동작
+        seatButtons.forEach(function (button) {
+            button.addEventListener('click', function () {
+                var selectedSeat = button.value;
+                var buttonColor = window.getComputedStyle(button).color;
+
+                if (buttonColor === 'rgb(255, 0, 0)') {
+                    alert('이미 예약된 좌석 ' + selectedSeat + '번입니다.');
+                } else {
+                    seatNum.textContent = selectedSeat;
+
+                    // 모든 좌석 버튼의 글씨 색상을 검정색으로 초기화
+                    seatButtons.forEach(function (resetButton) {
+                        if (window.getComputedStyle(resetButton).color !== 'rgb(255, 0, 0)') {
+                            resetButton.style.color = 'black';
+                        }
+                    });
+
+                    button.style.color = 'blue';
+                }
+            });
+        });
+
+        // 선택한 강의실과 사용 시간 업데이트
+        function updateRoomName() {
+            var selectedRoomNumber = document.querySelector('.roomNumber').value;
+            var selectedUseTime = document.getElementById('useTimeSelect').value;
+            var roomName = selectedRoomNumber + '강의실';
+
+            document.getElementById('roomName').textContent = roomName;
+            document.getElementById('roomTime').textContent = selectedUseTime;
+        }
+
+
+        // <main head>
         //logo
         var text = document.querySelector(".typing .text");
         var letters = [
@@ -297,3 +353,4 @@ for(let i = 0; i < 5; i++){
         console.log("click")
         window.location.href = "이인혜_notice_list.html"
     });
+        
