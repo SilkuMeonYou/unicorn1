333bfc0e0167611b9a00ebef702575ca4c98bef9
박신영 <skeicg12@gmail.com>
2023-10-26 11:49:25 +0900

    이벤트 서블릿, 장바구니 삭제버튼, 장바구니 담기랑 연결

---
+++ 박신영/1026/shopping_basket/cart_removeServlet.java
@@ -0,0 +1,34 @@
+package shopping_basket;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/cart_removeServlet")
+public class cart_removeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		HttpSession session = request.getSession();
+		PrintWriter out = response.getWriter();
+		String index = request.getParameter("cartProductIndex");
+		
+		List<CartDTO> cartList = (List<CartDTO>) session.getAttribute("cartList");
+		cartList.remove(Integer.parseInt(index));
+		
+		session.setAttribute("cartList", cartList);
+		
+		out.print("<script> alert('삭제되었습니다'); window.location='shopping_basket.jsp';"
+        		+ "</script>");
+	}
+
+}
